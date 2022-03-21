ecb318596e666194efd8cd48e2d67152d9df65cb
Viktor Lidholt <viktorlidholt@gmail.com>
2022-03-21 21:55:42 +0100

    Initial version

---
+++ docs/contribute.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 3
+---
+
+# Contribute
+
+Please make Serverpod better <3
---
+++ docusaurus.config.js
@@ -0,0 +1,147 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'serverpod', // Usually your GitHub org/user name.
+  projectName: 'serverpod', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          breadcrumbs: false,
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        respectPrefersColorScheme: true,
+        disableSwitch: true,
+      },
+      navbar: {
+        // title: 'My Site',
+        logo: {
+          alt: 'Serverpod Logo',
+          src: 'img/logo-horizontal.svg',
+          srcDark: 'img/logo-horizontal-dark.svg',
+          href: 'https://serverpod.dev',
+        },
+        items: [
+          // {
+          //   type: 'doc',
+          //   docId: 'intro',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          // {
+          //   type: 'doc',
+          //   docId: 'index',
+          //   label: 'Introduction',
+          //   position: 'left',
+          // },
+          // {
+          //   type: 'doc',
+          //   docId: 'getting-started',
+          //   label: 'Getting Started',
+          //   position: 'left',
+          // },
+          {
+            href: 'https://thehub.io/startups/serverpod',
+            label: 'Career',
+            position: 'right',
+          },
+          {
+            href: 'https://twitter.com/ServerpodDev',
+            label: 'Twitter',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/serverpod/serverpod',
+            label: 'GitHub',
+            position: 'right',
+          },
+          {
+            type: 'search',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        // links: [
+        //   {
+        //     title: 'Docs',
+        //     items: [
+        //       {
+        //         label: 'Tutorial',
+        //         to: '/docs/intro',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'Community',
+        //     items: [
+        //       {
+        //         label: 'Stack Overflow',
+        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //       },
+        //       {
+        //         label: 'Discord',
+        //         href: 'https://discordapp.com/invite/docusaurus',
+        //       },
+        //       {
+        //         label: 'Twitter',
+        //         href: 'https://twitter.com/docusaurus',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'More',
+        //     items: [
+        //       {
+        //         label: 'Blog',
+        //         to: '/blog',
+        //       },
+        //       {
+        //         label: 'GitHub',
+        //         href: 'https://github.com/facebook/docusaurus',
+        //       },
+        //     ],
+        //   },
+        // ],
+        copyright: `Copyright © ${new Date().getFullYear()} Serverpod authors.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['dart'],
+      },
+    }),
+};
+
+module.exports = config;
