dd7115406478a612d5c31a8ed180089bc3b4ea17
Damian Bast <me@damianbast.com>
2025-03-31 14:25:43 +0200

    Add tracking

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-TFEHVG44LQ',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
@@ -53,7 +57,8 @@ const config = {
           srcDark: 'img/logo-horizontal-dark.svg',
           href: 'https://serverpod.dev',
         },
-        items: [{
+        items: [
+          {
             type: 'docsVersionDropdown',
             position: 'left',
           },
@@ -93,48 +98,51 @@ const config = {
         additionalLanguages: ['dart', 'bash'],
       },
     }),
-  scripts: [{
-    src: "https://widget.kapa.ai/kapa-widget.bundle.js",
-    "data-website-id": "9ae02024-c2cf-4f58-b2d9-7ca3961dc1ef",
-    "data-project-name": "Serverpod",
-    "data-project-color": "#020F24",
-    "data-project-logo": "https://avatars.githubusercontent.com/u/48181558?s=200&v=4",
-    async: true,
-  }],
+  scripts: [
+    {
+      src: 'https://widget.kapa.ai/kapa-widget.bundle.js',
+      'data-website-id': '9ae02024-c2cf-4f58-b2d9-7ca3961dc1ef',
+      'data-project-name': 'Serverpod',
+      'data-project-color': '#020F24',
+      'data-project-logo':
+        'https://avatars.githubusercontent.com/u/48181558?s=200&v=4',
+      async: true,
+    },
+  ],
   plugins: [
     [
       '@docusaurus/plugin-client-redirects',
       {
-       redirects: [
-        {
-          // Moved in version 1.1.1
-          from: ['/concepts/authentication'],
-          to: '/concepts/authentication/setup',
-        },
-        {
-          // Moved in version 1.1.1 and 2.1.0
-          from: ['/tutorials', '/tutorials/videos'],
-          to: '/tutorials/first-app',
-        },
-        {
-          // Moved in version 1.2.0
-          from: ['/concepts/database-communication'],
-          to: '/concepts/database/connection',
-        },
-        {
-          // Moved in version 2.1.0
-          from: ['/insights'],
-          to: '/tools/insights',
-        },
-        {
-          // Moved in version 2.1.0
-          from: ['/roadmap'],
-          to: '/contribute',
-        }
-       ] 
-      }
-    ]
-  ]
+        redirects: [
+          {
+            // Moved in version 1.1.1
+            from: ['/concepts/authentication'],
+            to: '/concepts/authentication/setup',
+          },
+          {
+            // Moved in version 1.1.1 and 2.1.0
+            from: ['/tutorials', '/tutorials/videos'],
+            to: '/tutorials/first-app',
+          },
+          {
+            // Moved in version 1.2.0
+            from: ['/concepts/database-communication'],
+            to: '/concepts/database/connection',
+          },
+          {
+            // Moved in version 2.1.0
+            from: ['/insights'],
+            to: '/tools/insights',
+          },
+          {
+            // Moved in version 2.1.0
+            from: ['/roadmap'],
+            to: '/contribute',
+          },
+        ],
+      },
+    ],
+  ],
 };
 
 module.exports = config;
